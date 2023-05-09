8e11f1377ce3513cffcab11aab8a2aefed475569
Nilo <titolianchen3341@gmail.com>
2023-05-09 15:22:08 -0300

    VersãoFinal

--- script.js
+++ script.js
@@ -1,49 +1,21 @@
-// Lista de links cadastrados
-var links = [
-    "https://drive.google.com/file/d/1D7oT6xbwJMZOuemyPI0H_0pNxY84dZmJ/preview",
-    "https://drive.google.com/file/d/13TPhtzS3TnrHEZlwqdUmhjVoryUHXsfE/preview",
-    "https://drive.google.com/file/d/1GagAgdX7h1PhncvP1Snpn5OxQ7YrYS9r/preview",
-    /*
-    "imgs/FormsFeedback.gif",
+var imagens = [
+    "imgs/testepresencial.png",
+    "imgs/FormsFeedback.gif", 
+    "imgs/Keepers.png", 
     "imgs/vemAi.gif",
-    "https://dti.ag/LinksGestoAVista1",
-    "https://dti.ag/LinksGestoAVista2",
-    "https://dti.ag/LinksGestoAVista3",
-    "https://dti.ag/LinksGestoAVista4",
-    "https://dti.ag/LinksGestoAVista5",
-    "https://dti.ag/LinksGestoAVista6",
-    */
-];
-var indice = 0;
-var intervalo;
-function iniciarRotina() {
-    var iframe = document.getElementById("link-iframe");
-    iframe.src = links[indice];
-    indice = (indice + 1) % links.length;
-}
-function comecarRotina() {
-    iniciarRotina();
-    intervalo = setInterval(iniciarRotina, 18000);
-}
-function pararRotina() {
-    clearInterval(intervalo);
-}
-/*
-function definirTamanhoIframe() {
-    // Seleciona o elemento iframe
-    const iframe = document.getElementById('link-iframe');
+]; // array com as imagens
+var tempo = 18000; // tempo em milissegundos para trocar as imagens
+var indice = 0; // indice atual da imagem
 
-    // Cria um objeto Image para carregar a imagem
-    const img = new Image();
-
-    // Define a fonte da imagem como a fonte do iframe
-    img.src = iframe.src;
-
-    // Define a largura e altura do iframe com base no tamanho da imagem
-    img.onload = function () {
-        iframe.width = this.width;
-        iframe.height = this.height;
+function trocarImagem() {
+    document.getElementById("minha-imagem").src = imagens[indice];
+    indice++;
+    if (indice == imagens.length) {
+        indice = 0;
     }
+    setTimeout(trocarImagem, tempo);
 }
-*/
 
+window.onload = function () {
+    trocarImagem();
+};
